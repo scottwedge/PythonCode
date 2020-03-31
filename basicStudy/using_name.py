7accb5804ccc91dab322221bbccdf594d4dbd8f3
swedge <scott.wedge@gmail.com>
2020-03-30 23:17:36 -0400

    Fixes #1

--- basicStudy/using_name.py
+++ basicStudy/using_name.py
@@ -4,4 +4,4 @@
 if __name__ == '__main__':
     print 'This program is being run by itself'
 else:
-    print 'I am being immport from another module'
+    print 'I am being imported from another module'
